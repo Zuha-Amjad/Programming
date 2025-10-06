439fd48894e243d59c56ee5a21327a7840d12173
Zuha-Amjad <zuha291@gmail.com>
2025-10-07 00:18:53 +0500

    Cinema Case Study

--- Labs/Lab05/Cinema-Case Study/runScreenDemo.bat
+++
@@ -1,3 +0,0 @@
-javac -d out src\Seat.java src\SeatID.java src\Screen.java src\Cinema.java
-javac -cp out ScreenDemo.java
-java -cp out;. ScreenDemo
--- Labs/Lab05/Cinema-Case Study/runSeatDemo.bat
+++
@@ -1,3 +0,0 @@
-javac -d out src\Seat.java src\SeatID.java
-javac -cp out SeatDemo.java
-java -cp out;. SeatDemo
